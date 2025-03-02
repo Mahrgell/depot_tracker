00425deb9c97602dcf582013a61be02d8c6015e0
Marc Landgraf <mahrgell87@gmail.com>
2025-03-02 10:13:04 +0100

    feat: build_table takes any sort of iteratable, instead of only a vec

--- src/visu/table_builder.rs
+++ src/visu/table_builder.rs
@@ -3,7 +3,13 @@ use egui_extras::{Column, TableBuilder};
 
 use crate::properties::FormattedProperty;
 
-pub fn build_table<T>(ui: &mut egui::Ui, data: &Vec<T>, props: Vec<Box<dyn FormattedProperty<T>>>) {
+pub fn build_table<'a, T: 'a, I>(
+    ui: &mut egui::Ui,
+    data: I,
+    props: Vec<Box<dyn FormattedProperty<T>>>,
+) where
+    I: IntoIterator<Item = &'a T>,
+{
     TableBuilder::new(ui)
         .striped(true)
         .columns(Column::auto(), props.len())
@@ -15,11 +21,12 @@ pub fn build_table<T>(ui: &mut egui::Ui, data: &Vec<T>, props: Vec<Box<dyn Forma
             }
         })
         .body(|mut body| {
-            for d in data {
+            let mut itr = data.into_iter();
+            while let Some(d) = itr.next() {
                 body.row(18.0, |mut row| {
                     for p in &props {
                         row.col(|ui| {
-                            ui.label(p.format_data(d));
+                            ui.label(p.format_data(&d));
                         });
                     }
                 });
