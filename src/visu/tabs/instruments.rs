be572a6708bfa6d88bf244edc09ef0925f66ff7b
Marc Landgraf <mahrgell87@gmail.com>
2025-03-08 21:45:05 +0100

    feat: remove singular load local and save local buttons - auto save on AV load

--- src/visu/tabs/instruments.rs
+++ src/visu/tabs/instruments.rs
@@ -1,5 +1,7 @@
 use eframe::egui;
 
+const LOCAL_STORAGE: &str = "stock_data_storage/";
+
 use crate::{
     depot::Depot,
     instruments::InstrumentWrapped,
@@ -32,56 +34,51 @@ impl Instruments {
 
         stocks.sort_by_key(|(sym, _)| *sym);
 
-        egui::ComboBox::from_label("")
-            .selected_text(stocks[self.selected_index].0)
-            .show_ui(ui, |ui| {
-                for (index, &(sym, _)) in stocks.iter().enumerate() {
-                    if ui
-                        .selectable_label(self.selected_index == index, sym)
-                        .clicked()
-                    {
-                        self.selected_index = index;
-                        self.status_response = None;
+        ui.horizontal(|ui| {
+            egui::ComboBox::from_label("")
+                .selected_text(stocks[self.selected_index].0)
+                .show_ui(ui, |ui| {
+                    for (index, &(sym, _)) in stocks.iter().enumerate() {
+                        if ui
+                            .selectable_label(self.selected_index == index, sym)
+                            .clicked()
+                        {
+                            self.selected_index = index;
+                            self.status_response = None;
+                        }
                     }
-                }
-            });
+                });
+            if ui.button("Load all local").clicked() {
+                let lf = LocalFile::new(LOCAL_STORAGE.into());
+                let loaded_count = stocks
+                    .iter()
+                    .filter(|&(_, i)| i.update_data_with(&lf).is_ok())
+                    .count();
+                self.status_response =
+                    Some(format!("Loaded data for {} instruments.", loaded_count));
+            }
+        });
 
         ui.separator();
         ui.label(format!(
             "Number of data points: {}",
             stocks[self.selected_index].1.data().nb_data_points()
         ));
-        const LOCAL_STORAGE: &str = "stock_data_storage/";
-        ui.horizontal(|ui| {
-            if ui.button("Load local").clicked() {
-                let lf = LocalFile::new(LOCAL_STORAGE.into());
-                self.status_response =
-                    Some(match stocks[self.selected_index].1.update_data_with(&lf) {
-                        Ok((loaded_dp, new_dp)) => {
-                            format!("Loaded {} data points. ({} new)", loaded_dp, new_dp)
+
+        if ui.button("Load from AV").clicked() {
+            let av = AlphaVantage::from_apikey_file("alphavantage.dtkey").unwrap();
+            self.status_response =
+                Some(match stocks[self.selected_index].1.update_data_with(&av) {
+                    Ok((loaded_dp, new_dp)) => {
+                        if new_dp > 0 {
+                            let lf = LocalFile::new(LOCAL_STORAGE.into());
+                            let _ = stocks[self.selected_index].1.save_data_local(&lf);
                         }
-                        Err(e) => format!("Failed to load due to {:?}", e),
-                    });
-            }
-            if ui.button("Save local").clicked() {
-                let lf = LocalFile::new(LOCAL_STORAGE.into());
-                self.status_response =
-                    Some(match stocks[self.selected_index].1.save_data_local(&lf) {
-                        Ok(_) => "Successfully saved".into(),
-                        Err(_) => "Something failed when saving".into(),
-                    });
-            }
-            if ui.button("Load from AV").clicked() {
-                let av = AlphaVantage::from_apikey_file("alphavantage.dtkey").unwrap();
-                self.status_response =
-                    Some(match stocks[self.selected_index].1.update_data_with(&av) {
-                        Ok((loaded_dp, new_dp)) => {
-                            format!("Loaded {} data points. ({} new)", loaded_dp, new_dp)
-                        }
-                        Err(e) => format!("Failed to load due to {:?}", e),
-                    });
-            }
-        });
+                        format!("Loaded {} data points. ({} new)", loaded_dp, new_dp)
+                    }
+                    Err(e) => format!("Failed to load due to {:?}", e),
+                });
+        }
 
         if let Some(sr) = self.status_response.as_ref() {
             ui.label(sr);
